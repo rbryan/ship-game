c78247ee987f35ac1ef1e3d449e9ac4ac7b89052
Russell Bryan <bryanr@ou.edu>
2016-02-28 23:14:41 -0600

    trivial changes to misc.scm

--- misc.scm
+++ misc.scm
@@ -14,11 +14,9 @@
 				    ((_ . pattern) template)))))))
 
 
-(define-syntax do-times
-  (syntax-rules () ((do-times var count rest ...)
+(define-syntax-rule (do-times (var count) rest ...)
     (let loop ((var 0))
       (if (< var count)
         (begin
 	  rest ...
-	  (loop (+ var 1)))
-        #t)))))
+	  (loop (+ var 1))))))
