8b79489e71e7ee84e1ffaf1dc5d3bc50bb581707
Russell Bryan <bryanr@ou.edu>
2016-02-28 22:14:16 -0600

    We now have a client record

--- server.scm
+++ server.scm
@@ -1,5 +1,7 @@
 
 (import 
+	(server-client)
+
 	(srfi 69 basic-hash-tables) ;for hash-table->alist
 
   	(class java.net
@@ -13,27 +15,29 @@
 	       InputStreamReader))
 
 (define clients '())
-(define world '())
-(set! world (append world '((1 . 'hi))))
+(define world (make-hash-table))
+(hash-table-set! world 1 'hi)
 
 (define (server)
   (let ((listener ::ServerSocket (ServerSocket 9001)))
     (let loop ((socket ::Socket (listener:accept)))
       (synchronized clients
-	      (set! clients (append clients (list socket))))
+	      (set! clients (append clients (list (make-client socket)))))
       (loop (listener:accept)))))
 
 (define (world->string world)
-  (let ((output-string-port (open-output-string)))
+  (let ((world (hash-table->alist world))
+	(output-string-port (open-output-string)))
     (write world output-string-port)
     (get-output-string output-string-port)))
 
 (define (main)
   (let ((listener-thread (future (server))))
     (let main-loop  ((tick 0))
-      (for-each (lambda (client ::Socket)
-		  	(let ((world-string (world->string world))
-			      (output-port ::PrintWriter (PrintWriter (client:getOutputStream)))
+      (for-each (lambda (client)
+		  	(let* ((client ::Socket (client-socket client))
+			       (world-string (world->string world))
+			       (output-port ::PrintWriter (PrintWriter (client:getOutputStream)))
 			      (input-port ::BufferedReader (BufferedReader (InputStreamReader (client:getInputStream)))))
 			  (display client)
 			  (newline)
